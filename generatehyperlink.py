6b0f0c766bea70804cfebbe76579136c7a32b6b9
art <artrajtiwari@gmail.com>
2019-02-16 09:33:10 +0545

    lala

--- generatehyperlink.py
+++ generatehyperlink.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-f = open('GeratedLink.txt','w')
+f = open('GeratedLink.txt', 'w')
 requestObj = requests.get('https://techlekh.com/')
-soup = BeautifulSoup(requestObj.text,'html.parser')
+soup = BeautifulSoup(requestObj.text, 'html.parser')
 
-anchortags = soup.find_all('a',href = True)
+anchortags = soup.find_all('a', href=True)
 for a in anchortags:
-    f.write(str(a['href']) + '\n')
+    if a['href'] == '#':
+        print('')
+    else:
+        f.write(str(a['href']) + '\n')
